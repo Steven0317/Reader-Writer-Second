8488a1b3842662c7b1bb19a043b72e5d8ccc36cc
Steven Faulkner <sfaulkner1@gmail.com>
2018-11-13 12:51:54 -0500

    Changes to probabilty

--- reader-writer-2.c
+++ reader-writer-2.c
@@ -1,100 +1,104 @@
-#include <pthread.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sched.h>
-
-#define READERS 10
-#define WRITERS 5
-
-struct sched_param  param;
-struct sched_param  main_param;
-pthread_mutex_t     read_mutex      = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t     write_mutex     = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t     readTry_mutex   = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t     resource_mutex  = PTHREAD_MUTEX_INITIALIZER;
-pthread_attr_t      attribute_writer;
-pthread_attr_t      attribute_reader;
-pthread_t           reader_thread[READERS];
-pthread_t           writer_thread[WRITERS];
-static int          readcount;
-static int          writecount;
-static int          value;
-
-void* writers();
-void* readers();
-
-void proc_rt(int prio){
-    int min  = sched_get_priority_min(SCHED_RR);
-    int max  = sched_get_priority_max(SCHED_RR);
-    prio = prio > min ? prio : prio < max ? max : prio;
-    main_param.sched_priority = prio;
-    sched_setscheduler(pthread_self(), SCHED_RR, &main_param);
+#define NITERS 100
+ /* Sample code demonstrating reader-writers, using 2nd readers-writers
+ * solution
+ *
+ * Create NITERS agents, numbered from 1 to NITERS.  Each agent is
+ * randomly chosen to bea reader (probability 80%) or a writer
+ * (probability 20%).  Writers assign their ID to the global value.
+ * Readers read the global value.  Also insert random delays in agent
+ * generation so that writers can catch up to the readers.
+ */
+#include "csapp.h"
+#include <stdbool.h>
+ /* Uniform random number in [0.0,1.0] */
+static double uniform() {
+    return (double) random() / RAND_MAX;
 }
-
-int main(){
-    int ret;
-    printf("Readers-writers problem solve algorithm - writers-preference\n");
-
-    proc_rt(50);
-
-    pthread_attr_init(&attribute_writer);
-    pthread_attr_init(&attribute_reader);
-    ret = pthread_attr_getschedparam(&attribute_reader, &param);
-    param.sched_priority++;
-    ret = pthread_attr_setschedparam(&attribute_writer, &param);
-
-    for (int i = 0; i < WRITERS; i++)
-        pthread_create(&writer_thread[i], &attribute_writer, writers, NULL);
-    for (int i = 0; i < READERS; i++)
-        pthread_create(&reader_thread[i], &attribute_reader, readers, NULL);
-    
-    for (int i = 0; i < WRITERS; i++)
-        pthread_join(writer_thread[i], NULL);
-    for (int i = 0; i < READERS; i++)
-        pthread_join(reader_thread[i], NULL);
-    
+ bool choose_with_probability(double prob) {
+    return uniform() <=  prob;
+}
+ /* $begin reader1 */
+/* Global variables */
+int readcnt, writecnt; /* All initially = 0 */
+sem_t rmutex, wmutex, r, w;     /* All initially = 1 */
+ void init() {
+    readcnt = 0;
+    writecnt = 0;
+    Sem_init(&rmutex, 0, 1);
+    Sem_init(&wmutex, 0, 1);
+    Sem_init(&w, 0, 1);
+    Sem_init(&r, 0, 1);
+}
+ /* Get read access to data and read */
+int ireader(int *buf) 
+{
+    P(&r);   // Permission to attempt read
+    P(&rmutex);
+    readcnt++;
+    if (readcnt == 1) /* First in */
+	P(&w); // Block writers         
+    V(&rmutex);          
+    V(&r);
+     /* Critical section */
+    int v = *buf;
+     P(&rmutex);
+    readcnt--;
+    if (readcnt == 0) /* Last out */
+	V(&w);
+    V(&rmutex);
+    return v;
+}
+ /* Get write access to data and write */
+void iwriter(int *buf, int v) 
+{
+    P(&wmutex);
+    writecnt++;
+    if (writecnt == 1)
+	P(&r);  // Block readers
+    V(&wmutex);
+     P(&w);
+    /* Critical section */
+    *buf = v;
+    /* Writing happens  */ 
+    V(&w);
+     P(&wmutex);
+    writecnt--;
+    if (writecnt == 0)
+	V(&r);  // Enable readers
+    V(&wmutex);
+}
+ int global_value = 0;
+ void *rthread(void *vargp) {
+    int id = *(int *) vargp;
+    Free(vargp);
+    int v = ireader(&global_value);
+    printf("Reader %d read value %d\n", id, v);
+    return NULL;
+}
+ void *wthread(void *vargp) {
+    int id = *(int *) vargp;
+    Free(vargp);
+    iwriter(&global_value, id);
+    printf("Writer %d wrote value %d\n", id, id);
+    return NULL;
+}
+ int main(int argc, char *argv[]) {
+    int niters = NITERS;
+    if (argc > 1)
+	niters = atoi(argv[1]);
+    pthread_t tid[niters];
+    init();
+    int id;
+    for (id = 1; id <= niters; id++) {
+	bool doread = choose_with_probability(0.8);
+	void *vargp = Malloc(sizeof(int));
+	*(int *) vargp = id;
+	Pthread_create(&tid[id-1], NULL,
+		       doread ? rthread : wthread,
+		       vargp);
+    }
+    for (id = 1; id <= niters; id++) {
+	Pthread_join(tid[id-1], NULL);
+    }
     return 0;
-}
-
-void* readers(){
-    pthread_mutex_lock(&readTry_mutex);
-    pthread_mutex_lock(&read_mutex);
-    readcount++;
-    if (readcount == 1)
-        pthread_mutex_lock(&resource_mutex);
-    pthread_mutex_unlock(&read_mutex);
-    pthread_mutex_unlock(&readTry_mutex);
-
-    printf("\033[35;1m I'm reader number: %d\033[0m\n", readcount);
-    sleep(1);
-
-    pthread_mutex_lock(&read_mutex);
-    readcount--;
-    if (readcount == 0)
-        pthread_mutex_unlock(&resource_mutex);
-    pthread_mutex_unlock(&read_mutex);
-
-    pthread_exit(0);
-}
-
-void* writers(){
-    pthread_mutex_lock(&write_mutex);
-    writecount++;
-    if (writecount == 1)
-        pthread_mutex_lock(&readTry_mutex);
-    pthread_mutex_unlock(&write_mutex);
-    
-    pthread_mutex_lock(&resource_mutex);
-    printf("\033[31;1m I'm writer number: %d\033[0m\n", writecount);
-    pthread_mutex_unlock(&resource_mutex);
-    sleep(1);
-
-    pthread_mutex_lock(&write_mutex);
-    writecount--;
-    if (writecount == 0)
-        pthread_mutex_unlock(&readTry_mutex);
-    pthread_mutex_unlock(&write_mutex);
-
-    pthread_exit(0);
 }
